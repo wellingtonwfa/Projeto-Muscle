901d9f1c1c0dd2cc2e4f19ca87e70213420474c7
wellingtonwfa <wellingtonwfa@hotmail.com>
2017-08-02 17:42:20 -0300

    inclusao da tela de refeicao

--- WindowsApplication1VisualBasic/WindowsApplication1VisualBasic/CadastroRefeicao.vb
+++ WindowsApplication1VisualBasic/WindowsApplication1VisualBasic/CadastroRefeicao.vb
@@ -1,3 +1,90 @@
 ﻿Public Class CadastroRefeicao
+    Dim intIdRefeicao As Integer
 
+    Private Sub CadastroRefeicao_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BuscaDados()
+    End Sub
+    Private Sub BuscaDados()
+
+        Dim sql = "SELECT IdTipoRefeicao, NomeRefeicao FROM muscle.tb_tiporefeicao"
+
+        Dim dt As DataTable = DAL.AcessoBD.ExecutarComando(sql, CommandType.Text, Nothing, DAL.AcessoBD.TipoDeComando.ExecuteDataTable)
+
+        Me.grdRefeicao.DataSource = dt
+
+    End Sub
+
+    Private Sub imbSalvar_Click(sender As Object, e As EventArgs) Handles imbSalvar.Click
+        Salvar()
+    End Sub
+    Private Sub Salvar()
+        Dim strRefeicao As String = ""
+        Dim sql As String = ""
+
+        strRefeicao = RTrim(Me.txtRefeicao.Text)
+
+        If strRefeicao = "" Then
+            MessageBox.Show("Informe o nome da Refeição!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+
+        If intIdRefeicao = 0 Then
+
+            For Each dtObjetvo As DataGridViewRow In grdRefeicao.Rows
+                If dtObjetvo.Cells(1).Value = strRefeicao Then
+                    MessageBox.Show("Esse Refeição já existe!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    Exit Sub
+                End If
+            Next
+
+            sql = "insert tb_tiporefeicao(NomeRefeicao) values('" + strRefeicao + "')"
+        Else
+
+            sql = "update muscle.tb_tiporefeicao set NomeRefeicao = '" + strRefeicao + "' where IdTipoRefeicao = " + CStr(intIdRefeicao)
+
+        End If
+
+        Dim dt As DataSet = DAL.AcessoBD.ExecutarComando(sql, CommandType.Text, Nothing, DAL.AcessoBD.TipoDeComando.ExecuteDataSet)
+
+        Limpar()
+
+
+    End Sub
+
+    Private Sub imbLimpar_Click(sender As Object, e As EventArgs) Handles imbLimpar.Click
+        Limpar()
+    End Sub
+    Private Sub Limpar()
+
+        intIdRefeicao = 0
+        Me.txtRefeicao.Text = ""
+        BuscaDados()
+
+    End Sub
+
+    Private Sub imbExcluir_Click(sender As Object, e As EventArgs) Handles imbExcluir.Click
+        Excluir()
+    End Sub
+    Private Sub Excluir()
+        Dim sql As String = ""
+
+        If intIdRefeicao = 0 Then
+            MessageBox.Show("Selecione um Alimento na grid!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Exit Sub
+        End If
+
+        sql = "delete from muscle.tb_tiporefeicao where IdTipoRefeicao = " + CStr(intIdRefeicao)
+
+        Dim dt As DataSet = DAL.AcessoBD.ExecutarComando(sql, CommandType.Text, Nothing, DAL.AcessoBD.TipoDeComando.ExecuteDataSet)
+
+        Limpar()
+
+
+    End Sub
+
+    Private Sub grdRefeicao_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles grdRefeicao.CellContentClick
+
+        intIdRefeicao = grdRefeicao.Rows(e.RowIndex).Cells(0).Value.ToString()
+        Me.txtRefeicao.Text = grdRefeicao.Rows(e.RowIndex).Cells(1).Value.ToString()
+
+    End Sub
 End Class
